0a21cb0dd6f981905ae17178933a4a2c2e45d4ac
nischal <nischal.bhatt.87@gmail.com>
2022-03-08 10:30:53 +0800

    changes

--- src/main/java/com/example/demo/WelcomeBean.java
+++ src/main/java/com/example/demo/WelcomeBean.java
@@ -2,6 +2,15 @@ package com.example.demo;
 
 public class WelcomeBean {
  private String message;
+ private String anothermessage = "123";
+
+public String getAnothermessage() {
+	return anothermessage;
+}
+
+public void setAnothermessage(String anothermessage) {
+	this.anothermessage = anothermessage;
+}
 
 public WelcomeBean(String message) {
 	super();
